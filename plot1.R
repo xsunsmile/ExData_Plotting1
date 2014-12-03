9d7cdabd7ae11b15bb414c76f954964e6a7ffa83
Hao SUN <hasun@zendesk.com>
2014-12-03 09:28:14 -0800

    use sqldf to load date in

--- plot1.R
+++ plot1.R
@@ -1,6 +1,11 @@
+if(!require("sqldf")) {
+  install.packages("sqldf")
+}
+library(sqldf)
 png("plot1.png", width = 480, height = 480)
+
 par(mar = c(5,5,4,4))
-target <- household_power_consumption[household_power_consumption$Date == '1/2/2007' | household_power_consumption$Date == '2/2/2007',]
-target <- transform(target, Global_active_power = as.numeric(as.character(Global_active_power)))
+target <- read.csv2.sql("/Users/hsun/Documents/DataMining/LearnR/household_power_consumption.txt", "select * from file where Date in ('1/2/2007','2/2/2007')")
 hist(target$Global_active_power, col = 'red', xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power')
+
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,7 +1,12 @@
+if(!require("sqldf")) {
+  install.packages("sqldf")
+}
+library(sqldf)
 png("plot2.png", width = 480, height = 480)
+
 par(mar = c(5,5,4,4))
-target <- household_power_consumption[household_power_consumption$Date == '1/2/2007' | household_power_consumption$Date == '2/2/2007',]
-target <- transform(target, Global_active_power = as.numeric(as.character(Global_active_power)))
+target <- read.csv2.sql("/Users/hsun/Documents/DataMining/LearnR/household_power_consumption.txt", "select * from file where Date in ('1/2/2007','2/2/2007')")
 target$DateTime <- strptime(paste(target$Date, target$Time, sep = ' '), format = "%d/%m/%Y %H:%M:%S")
 plot(target$DateTime, target$Global_active_power, type = 'l', ylab = 'Global Active Power (kilowatts)', xlab = '')
+
 dev.off()
